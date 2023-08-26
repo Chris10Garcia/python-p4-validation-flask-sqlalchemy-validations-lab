30e4ff9dd57c3ea8a68ac734cd9e143579214bcf
Christian Garcia <bones192nd@gmail.com>
2023-08-26 15:43:51 -0400

    Mostly used validates() but also used some constraint checking

--- server/models.py
+++ server/models.py
@@ -1,14 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
+import re
 
 class Author(db.Model):
     __tablename__ = 'authors'
     # Add validations and constraints 
 
+    @validates('name')
+    def check_name(self, key, address):
+        if address == "":
+            raise ValueError("Please input a name")
+        if Author.query.filter(Author.name == address).first():
+            raise ValueError("Name already exist in the DB")        
+        return address
+    
+    @validates('phone_number')
+    def check_phone(self, key, address):
+        if len(address) != 10:
+            raise ValueError("Please input a correct 10 digit number")
+        return address
+    
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    phone_number = db.Column(db.String(10))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
@@ -17,11 +32,51 @@ class Author(db.Model):
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
+    
+    @validates("title")
+    def check_title(self, key, address):
+        if address == "":
+            raise ValueError("Please input a title")
+        # return address
+        clickbait_titles = [
+            "Won't Believe",
+            "Secret",
+            "Top",
+            "Guess"
+        ]
+
+        for title in clickbait_titles:
+            pattern = rf"{title}"
+            regex = re.compile(pattern)
+            match = regex.search(address)
+
+            if not match:
+                raise ValueError("Title is not click-baity enough")
+        
+        return address
+
+    @validates("content")
+    def check_content(self, key, address):
+        if len(address) < 250:
+            raise ValueError("Content length is insufficient")
+        return address
+
+    @validates("summary")
+    def check_summary(self, key, address):
+        if len(address) >= 250:
+            raise ValueError ("Summary is too long")
+        return address
+    
+    @validates("category")
+    def check_category(self, key, address):
+        if address != "Fiction" and address != "Non-Fiction":
+            raise ValueError ("Category should be Fiction or Non-Fiction")
+        return address
+
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    content = db.Column(db.String)
+    content = db.Column(db.String(250))
     category = db.Column(db.String)
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
